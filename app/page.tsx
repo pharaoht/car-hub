30e6b294d621f0175b07f366d3aa8a35a53f4aea
pharaoh <pharaohmanson@gmail.com>
2024-03-02 14:08:42 -0500

    car hub display website complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,72 @@
-import Image from "next/image";
+import CustomFilter from "@/components/CustomFilter";
+import { Hero } from "@/components/Hero";
+import SearchBar from "@/components/SearchBar";
+import { fetchCars } from "@/utils";
+import CarCard from "@/components/CarCard";
+import { fuels, yearsOfProduction } from "@/constants";
+import ShowMore from "@/components/ShowMore";
 
-export default function Home() {
+export default  async function Home({ searchParams }) {
+
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2023,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || ''
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="overflow-hidden">
+      <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id='discover'>
 
+        <div className="home__text-container">
+          <h1 className="text-4x1 hero__subtitle font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+
+          </div>
+        </div>
+
+        {
+          !isDataEmpty ? 
+          (
+            <section>
+              <div className="home__cars-wrapper">
+                {
+                  allCars?.map((car) => (
+                    <CarCard car={car} key={car.id} />
+                  ))
+                }
+              </div>
+
+              <ShowMore 
+                pageNumber={(searchParams.limit || 10) / 10}
+                isNext={(searchParams.limit || 10) > allCars.length}
+              />
+            </section>
+          ) 
+          : 
+          (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold"> Oops, Nothing</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
+        }
+
+
+      </div>
     </main>
   );
 }
---
+++ utils/index.tsx
@@ -0,0 +1,66 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps){
+
+    const { manufacturer, model, limit, fuel, year } = filters;
+
+    const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`;
+
+    const header = {
+		'X-RapidAPI-Key': 'dd84fdbbedmshd3dbcecf3a43645p185d78jsn31a55ae40be2',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	};
+
+    const response = await fetch(url, { headers: header });
+
+    const result = await response.json();
+
+    return result;
+};
+
+export const calculateCarRent = ( city_mpg: number, year: number ) => {
+
+    const basePricePerDay = 50;
+
+    const mileageFactor = 0.1;
+
+    const ageFactor = 0.05;
+
+    const mileageRate = city_mpg * mileageFactor;
+
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+}
+
+export const generateCarImageUrl = ( car: CarProps, angle?: string) => {
+
+    const url = new URL('https://cdn.imagin.studio/getimage')
+
+    const { make, year, model } = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(' ')[0]);
+    url.searchParams.append('zoomType', 'fullScreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`
+
+
+
+}
+
+export const updateSearchParams = (type: string, value:string) => {
+
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value);
+
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+
+    return newPathName;
+}
